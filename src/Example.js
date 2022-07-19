1ddbac6320cea713ab45f2aa6ee1997697d89ed2
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-07-19 15:25:53 +0200

    Update Example.js

--- src/Example.js
+++ src/Example.js
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
   console.log("rerendered!")
-  
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div>
       <p>Count is {count}</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={increment}>
        Increase by 1
       </button>
-      <button onClick={() => setCount(count - 1)}>
+      <button onClick={decrement}>
       Decrease by 1
       </button>
     </div>
